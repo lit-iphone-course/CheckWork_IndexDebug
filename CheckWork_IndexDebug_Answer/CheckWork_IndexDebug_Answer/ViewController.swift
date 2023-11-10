c241730ee9bf6e57a2a9d39703a32f20310fa9f3
gil0304 <ryogo5.0304@gmail.com>
2023-11-10 18:56:00 +0900

    add: checkwork&answer

---
+++ CheckWork_IndexDebug_Answer/CheckWork_IndexDebug_Answer/ViewController.swift
@@ -0,0 +1,75 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var index: Int = 0
+    
+    @IBOutlet var label: UILabel!
+    
+    //MARK: 6番目のボタンを押すと落ちる
+    var nameArray: [String] = ["iPhone", "Android", "WebS", "WebD", "Game", "Movie"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        label.text = nameArray[index]
+       
+    //MARK: viewDidLoadの閉じるカッコ( } )がない。
+    }
+    
+    @IBAction func next() {
+        
+        //MARK: nextを5回連続で押すと落ちる
+        if index < 5 {
+            
+            index = index + 1
+            label.text = nameArray[index]
+        }
+    }
+    
+    @IBAction func previous() {
+        
+        if index > 0 {
+            
+            index = index - 1
+            label.text = nameArray[index]
+        }
+    }
+    
+    @IBAction func first() {
+        
+        index = 0
+        label.text = nameArray[index]
+    }
+    
+    @IBAction func second() {
+        
+        index = 1
+        label.text = nameArray[index]
+    }
+    
+    @IBAction func third() {
+        
+        index = 2
+        label.text = nameArray[index]
+    }
+    
+    @IBAction func fourth() {
+        
+        index = 3
+        label.text = nameArray[index]
+    }
+    
+    @IBAction func fifth() {
+        
+        index = 4
+        label.text = nameArray[index]
+    }
+    
+    @IBAction func sixth() {
+        
+        index = 5
+        label.text = nameArray[index]
+    }
+}
+
